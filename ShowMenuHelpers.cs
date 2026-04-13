263b0196b069914a0ade1c307ee5e4978d72e699
Kitsolitelu <ST10486804@rcconnect.edu.za>
2026-04-13 16:24:12 +0200

    3. Added menu navigation and pages

---
+++ ShowMenuHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CyberBot_PROG_Prac1
+{
+    internal static class ShowMenuHelpers
+    {
+
+
+        public static void ShowMenu()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("\n***** CYBERSECURITY AWARENESS BOT MENU *****");
+            Console.WriteLine("1. Password Safety");
+            Console.WriteLine("2. Phishing");
+            Console.WriteLine("3. Safe Browsing");
+            Console.WriteLine("4. Exit");
+
+            Console.ResetColor();
+            CyberBot_PROG_Prac1.UI.TypeText("You can type a number OR  keyword like 'phishing'\n");
+        }
+    }
+}
