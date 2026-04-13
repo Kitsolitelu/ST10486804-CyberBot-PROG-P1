50c9d11b5d007aaee42dd7d200b83447bd52a5a2
Kitsolitelu <ST10486804@rcconnect.edu.za>
2026-04-13 16:23:05 +0200

    2. Implemented user input and validation

---
+++ GetUserInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberBot_PROG_Prac1
+{
+    internal class GetUserInput
+    {
+        
+         public static string GetUsername()
+        {
+            UI.TypeText("What is your name?");
+            string name = Console.ReadLine();
+
+            while (string.IsNullOrWhiteSpace(name))
+            {
+                UI.TypeText("Please enter a valid name:");
+                name = Console.ReadLine();
+            }
+
+            return name;
+        }
+    }
+}
