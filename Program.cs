f69a9a3c448ee5e18f6dd3f1eecd959f46c5606d
Kitsolitelu <ST10486804@rcconnect.edu.za>
2026-04-13 16:30:17 +0200

    Implemend main structure for the program to start running

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Cybersecurity awareness bot project - ST10486804
+using CyberBot_PROG_Prac1;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ChatBot bot = new ChatBot();
+        bot.Start();
+        
+    }
+
+}
+
+
